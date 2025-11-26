ff6dd69a76e0f5a729170da8ffb0c9d931977a65
Genie128 <genie11530@gmail.com>
2025-11-27 01:22:40 +0800

    Update YarnBallIcon SVG dimensions and paths

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -2,25 +2,29 @@
 import React from 'react';
 
 export const YarnBallIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="1.5"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    viewBox="0 0 64 64" 
+    fill="none" 
+    stroke="currentColor" 
+    strokeWidth="2" 
+    strokeLinecap="round" 
+    strokeLinejoin="round" 
     {...props}
   >
-    <path d="M12 5C9.24 5 7 7.24 7 10s2.24 5 5 5 5-2.24 5-5" />
-    <path d="M12 5a7 7 0 0 0-7 7" />
-    <path d="M12 15a7 7 0 0 0 7-7" />
-    <path d="M12 5a7 7 0 0 1 7 7" />
-    <path d="M12 15a7 7 0 0 1-7-7" />
-    <path d="M5 10a7 7 0 0 0 7 7" />
-    <path d="M19 10a7 7 0 0 0-7 7" />
-    <path d="M5 10a7 7 0 0 1 7-7" />
-    <path d="M19 10a7 7 0 0 1-7-7" />
+    <path d="M45.5,23.5l-27-13" />
+    <path d="M48.5,32.5l-24,19" />
+    <circle cx="47" cy="22" r="2" fill="currentColor" />
+    <circle cx="50" cy="31" r="2" fill="currentColor" />
+    <path d="M32,49c-8.8,0-16-7.2-16-16s7.2-16,16-16s16,7.2,16,16" />
+    <path d="M32,17c-8.8,0-16,7.2-16,16" />
+    <path d="M32,49c8.8,0,16-7.2,16-16" />
+    <path d="M16,33c0-8.8,7.2-16,16-16" />
+    <path d="M48,33c0,8.8-7.2,16-16,16" />
+    <path d="M24,25c0,4.4,3.6,8,8,8" />
+    <path d="M40,41c0-4.4-3.6-8-8-8" />
+    <path d="M16,33c0-4.4,3.6-8,8-8" />
+    <path d="M21,43a4,4,0,0,1,4-4h2" />
   </svg>
 );
 
