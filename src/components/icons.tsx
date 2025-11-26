2bafb3c2e13ca6f4208819c90dcbbdd57c4c852c
Genie128 <genie11530@gmail.com>
2025-11-27 01:25:46 +0800

    Remove RulerIcon component
    Removed the RulerIcon component from icons.tsx.

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -27,23 +27,3 @@ export const YarnBallIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => 
     <path d="M21,43a4,4,0,0,1,4-4h2" />
   </svg>
 );
-
-export const RulerIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-    <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        viewBox="0 0 24 24" 
-        fill="none" 
-        stroke="currentColor" 
-        strokeWidth="2" 
-        strokeLinecap="round" 
-        strokeLinejoin="round" 
-        {...props}
-    >
-        <path d="M21.3 15.3a2.4 2.4 0 0 1 0 3.4l-2.6 2.6a2.4 2.4 0 0 1-3.4 0L3 8.7a2.4 2.4 0 0 1 0-3.4l2.6-2.6a2.4 2.4 0 0 1 3.4 0L12 5.7" />
-        <path d="m15 12-2-2" />
-        <path d="m2.3 21.7.9-.9" />
-        <path d="m8 16-2-2" />
-        <path d="m11 13-2-2" />
-        <path d="m19 5-2-2" />
-    </svg>
-);
